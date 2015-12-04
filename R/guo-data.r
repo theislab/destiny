9284478732f5430f6dde872339159a295774ba2c
Philipp Angerer <philipp.angerer@helmholtz-muenchen.de>
2015-12-04 13:29:24 +0000

    updated dataset docs
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/destiny@111205 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/guo-data.r
+++ R/guo-data.r
@@ -1,17 +1,20 @@
 #' Guo at al. mouse embryonic stem cell qPCR data
 #' 
-#' Gene expression data of 48 genes and an annotation column \code{$division} containing the number of divisions after which the cells were harvested.
+#' Gene expression data of 48 genes and an annotation column \code{$num.cells} containing the cell stage at which the embryos were harvested.
 #' 
-#' The data is normalized using the mean of two housekeeping genes and the LoD is set to 15.
+#' The data is normalized using the mean of two housekeeping genes.
+#' The difference between \code{guo} and \code{guo.norm} is the LoD being set to 10 in the former, making it usable with the \code{censor.val} parameter of \link{DiffusionMap}.
 #' 
 #' @return a \eqn{429 \times 49} \code{data.frame} with the first 48 columns being qPCR Ct values and the last one the "divisions" annotation.
 #' 
-#' @aliases data(guo) data-guo guo
-#' @name data-guo
+#' @aliases data:guo data:guo.norm guo guo.norm
+#' @name guo
 #' @docType data
 #' @author Guoji Guo, Mikael Huss, Guo Qing Tong, Chaoyang Wang, Li Li Sun, Neil D. Clarke, Paul Robson \email{robsonp@@gis.a-star.edu.sg}
 #' @references \url{http://www.sciencedirect.com/science/article/pii/S1534580710001103}
 #' @keywords data
-#' @usage data(guo)
+#' @usage
+#' data(guo)
+#' data(guo.norm)
 #' @format A data frame with 492 rows and 49 variables, the first 48 of which are Ct values for gene expression data
 NULL
