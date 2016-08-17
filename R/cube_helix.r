345c0a5c4d1b5fe595888819a347af573d69c998
Philipp Angerer <philipp.angerer@helmholtz-muenchen.de>
2016-08-17 09:44:54 +0000

    extracted color legend and cube helix
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/destiny@120200 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/cube_helix.r
@@ -0,0 +1,38 @@
+#' Sequential color palette using the cube helix system
+#' 
+#' Creates a perceptually monotonously decreasing (or increasing) lightness color palette with different tones.
+#' 
+#' @param n        Number of colors to return (default: 6)
+#' @param start    Hue to start helix at (\eqn{\textrm{start} \in [0,3]}, default: 0)
+#' @param r        Number of rotations of the helix. Can be negative. (default: 0.4)
+#' @param hue      Saturation. 0 means greyscale, 1 fully saturated colors (default: 0.8)
+#' @param gamma    Emphasize darker (gamma < 1) or lighter (gamma > 1) colors (default: 1)
+#' @param light    Lightest lightness (default: 0.85)
+#' @param dark     Darkest lightness (default: 0.15)
+#' @param reverse  logical. If TRUE, reverse lightness (default: FALSE)
+#' 
+#' @return A \code{character} vector of hex colors with length \code{n}
+#' 
+#' @examples
+#' palette(cube_helix())
+#' image(matrix(1:6), col = 1:6, pch = 19, axes = FALSE)
+#' 
+#' cr <- colorRamp(cube_helix(12, r = 3))
+#' r <- runif(100)
+#' plot(1:100, r, col = rgb(cr(r), max = 255), type = 'b', pch = 20)
+#' 
+#' @importFrom grDevices rgb
+#' @export
+cube_helix <- function(n = 6, start = 0, r = .4, hue = .8, gamma = 1, light = .85, dark = .15, reverse = FALSE) {
+	M <- matrix(c(-.14861, -.29227, 1.97294,
+								1.78277, -.90649, 0), ncol = 2)
+	lambda <- seq(light, dark, length.out = n)
+	if (reverse) lambda <- rev(lambda)
+	l <- rep(lambda ^ gamma, each = 3)
+	phi <- 2 * pi * (start/3 + r * lambda)
+	t <- rbind(cos(phi), sin(phi))
+	out <- l + hue * l * (1 - l)/2 * (M %*% t)
+	out <- pmin(pmax(out, 0), 1)
+	out <- apply(out, 2, function(x) rgb(x[[1]], x[[2]], x[[3]]))
+	out
+}
