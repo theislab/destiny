bd0b48a5749972124d144b7d130987bba635dd27
Philipp Angerer <philipp.angerer@helmholtz-muenchen.de>
2016-09-12 13:26:28 +0000

    removed brewer.pal call from .onLoad
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/destiny@120855 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/aaa.r
+++ R/aaa.r
@@ -1,9 +1,9 @@
 bad_default_palette <- c('black', 'red', 'green3', 'blue', 'cyan', 'magenta', 'yellow', 'gray')
+#this is Set3, but RColorBrewer doesn’t work in .onLoad due to using rgb() unconditionally
+gud_default_palette <- c('#8DD3C7', '#FFFFB3', '#BEBADA', '#FB8072', '#80B1D3', '#FDB462', '#B3DE69', '#FCCDE5', '#D9D9D9', '#BC80BD', '#CCEBC5', '#FFED6F')
 
-#' @importFrom scales brewer_pal
 .onLoad <- function(libname, pkgname) {
 	if (identical(palette(), bad_default_palette)) {
-		better_default_palette <- brewer_pal(palette = 'Set3')
-		palette(better_default_palette(12L))
+		palette(gud_default_palette)
 	}
 }
