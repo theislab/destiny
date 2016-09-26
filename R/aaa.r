35434c01237d07d62b2cc7390dfe555181cccf76
Philipp Angerer <philipp.angerer@helmholtz-muenchen.de>
2016-09-26 16:17:10 +0000

    improve colormap
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/destiny@121399 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/aaa.r
+++ R/aaa.r
@@ -1,6 +1,6 @@
 bad_default_palette <- c('black', 'red', 'green3', 'blue', 'cyan', 'magenta', 'yellow', 'gray')
-#this is Set3, but RColorBrewer doesn’t work in .onLoad due to using rgb() unconditionally
-gud_default_palette <- c('#8DD3C7', '#FFFFB3', '#BEBADA', '#FB8072', '#80B1D3', '#FDB462', '#B3DE69', '#FCCDE5', '#D9D9D9', '#BC80BD', '#CCEBC5', '#FFED6F')
+#this is a reshuffled Set3, but RColorBrewer doesn’t work in .onLoad due to using rgb() unconditionally
+gud_default_palette <- c('#8DD3C7', '#FFED6F', '#BEBADA', '#FB8072', '#80B1D3', '#FDB462', '#B3DE69', '#BC80BD', '#FCCDE5', '#D9D9D9', '#CCEBC5', '#FFFFB3')
 
 .onLoad <- function(libname, pkgname) {
 	if (identical(palette(), bad_default_palette)) {
