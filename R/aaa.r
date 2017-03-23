a37e8acd34c9f4f00ad9bb6cf31d95dab5f03151
Philipp Angerer <philipp.angerer@helmholtz-muenchen.de>
2017-03-23 10:23:48 +0000

    fix superfluous window opening
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/destiny@127649 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/aaa.r
+++ R/aaa.r
@@ -2,8 +2,11 @@ bad_default_palette <- c('black', 'red', 'green3', 'blue', 'cyan', 'magenta', 'y
 #this is a reshuffled Set3, but RColorBrewer doesn’t work in .onLoad due to using rgb() unconditionally
 gud_default_palette <- c('#8DD3C7', '#FFED6F', '#BEBADA', '#FB8072', '#80B1D3', '#FDB462', '#B3DE69', '#BC80BD', '#FCCDE5', '#D9D9D9', '#CCEBC5', '#FFFFB3')
 
+# do not use .Call.graphics like grDevices::palette
+set_palette <- function(v) .Call(grDevices:::C_palette, v)
+
 .onLoad <- function(libname, pkgname) {
 	if (identical(palette(), bad_default_palette)) {
-		palette(gud_default_palette)
+		set_palette(gud_default_palette)
 	}
 }
