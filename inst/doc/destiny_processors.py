4886035d49f0b465d4f9340165d81ebe954e0ff8
Philipp Angerer <philipp.angerer@helmholtz-muenchen.de>
2017-07-17 14:26:33 +0000

    use .install_extras
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/destiny@131281 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/doc/destiny_processors.py
+++
@@ -1,7 +0,0 @@
-from nbconvert.preprocessors.base import Preprocessor
-from pygments.formatters import LatexFormatter
-
-class LatexTracPreprocessor(Preprocessor):
-	def preprocess(self, nb, resources):
-		resources['latex']['pygments_definitions'] = LatexFormatter(style='trac').get_style_defs()
-		return nb, resources
