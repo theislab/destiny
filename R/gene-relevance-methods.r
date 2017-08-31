56ad103529c70f50c41c17eea2a4e35aa93d8585
Philipp A <flying-sheep@web.de>
2017-08-31 16:20:23 +0200

    Improved docs for featureNames.GeneRelevance

--- R/gene-relevance-methods.r
+++ R/gene-relevance-methods.r
@@ -7,6 +7,9 @@ NULL
 #' \code{featureNames <- ...} Can be used to set the gene names used for plotting
 #' (e.g. if the data contains hardly readably gene or transcript IDs)
 #' 
+#' @param object  \code{\link{GeneRelevance}} object
+#' @param value   A text vector (\code{\link{character}} or \code{\link{factor}})
+#' 
 #' @seealso \code{\link{gene_relevance}}, \link{Gene Relevance plotting}
 #' 
 #' @examples
@@ -15,6 +18,9 @@ NULL
 #' featureNames(gr)[[37]] <- 'Id2 (suppresses differentiation)'
 #' # now plot it with the changed gene name(s)
 #' 
+#' @aliases
+#' featureNames,GeneRelevance-method
+#' featureNames<-,GeneRelevance,characterOrFactor-method
 #' @name Gene Relevance methods
 #' @export
 setMethod('featureNames', 'GeneRelevance', function(object) colnames(object@exprs))
