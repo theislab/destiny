08128a09ec7b16578fc3f6a93caaf12cca3a9425
Philipp A <flying-sheep@web.de>
2017-10-26 13:08:58 +0200

    WIP ggplot2 switch

--- R/diffusionmap-plotting.r
+++ R/diffusionmap-plotting.r
@@ -32,7 +32,8 @@ NULL
 #' @param col_na       Color for \code{NA} in the data. specify \code{NA} to hide.
 #' @param plot_more    Function that will be called while the plot margins are temporarily changed
 #'                     (its \code{p} argument is the rgl or scatterplot3d instance or NULL,
-#'                     its \code{rescale} argument is \code{NULL} or of the shape \code{list(from = c(a, b), to = c(c,d))})
+#'                     its \code{rescale} argument is \code{NULL} or of the shape \code{list(from = c(a, b), to = c(c,d))}).
+#'                     In case of 2d plotting, it should take and return a ggplot2 object.
 #' 
 #' @return The return value of the underlying call is returned, i.e. a scatterplot3d or rgl object.
 #' 
@@ -44,17 +45,24 @@ NULL
 #' 
 #' @importFrom graphics par axis plot plot.new
 #' @importFrom grDevices palette
+#' @importFrom ggplot2 ggplot aes aes_string
+#' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 theme theme_minimal element_blank element_line element_text element_rect
+#' @importFrom ggplot2 scale_colour_identity scale_colour_manual scale_colour_gradientn
+#' @importFrom ggplot2 scale_x_continuous scale_y_continuous
+#' @importFrom ggplot2 guide_colourbar guide_legend
+#' @importFrom ggthemes geom_rangeframe extended_range_breaks
 #' 
 #' @name plot.DiffusionMap
 #' @export
 plot.DiffusionMap <- function(
 	x, dims = 1:3,
-	new_dcs = NULL, new_data = NULL,
+	new_dcs = if (!is.null(new_data)) dm_predict(x, new_data),
+	new_data = NULL,
 	col = NULL, col_by = NULL, col_limits = NULL,
 	col_new = 'red',
 	pal = NULL, pal_new = NULL,
 	...,
-	mar = NULL,
 	ticks = FALSE,
 	axes = TRUE,
 	box = FALSE,
@@ -62,7 +70,7 @@ plot.DiffusionMap <- function(
 	interactive = FALSE,
 	draw_legend = !is.null(col_by) || (length(col) > 1 && !is.character(col)),
 	consec_col = TRUE, col_na = 'grey',
-	plot_more = function(p, ..., rescale = NULL) NULL
+	plot_more = function(p, ..., rescale = NULL) p
 ) {
 	dif <- x
 	
@@ -73,36 +81,16 @@ plot.DiffusionMap <- function(
 			stop('Only 3d plots can be made interactive')
 	}
 	
-	if (!is.null(new_data)) {
-		if (!is.null(new_dcs)) stop('either provide new_data or new_dcs')
-		new_dcs <- dm_predict(dif, new_data)
-	}
-	
 	if (!is.null(col) && !is.null(col_by)) stop('Only specify one of col or col_by')
+	if (!is.null(col_by)) col <- extract_col(dataset(dif), col_by)
+	if (is.null(col)) col <- ''  # just a single color
+	continuous <- is.double(col)
+	col_legend <- if (continuous && !is.null(col_limits)) col_limits else col
 	
-	col_default <- is.null(col) && is.null(col_by)
-	if (col_default) col <- par('col')
-	
-	#get col from data
-	if (!is.null(col_by)) {
-		col <- extract_col(dataset(dif), col_by)
-		if (is.null(col)) stop('no "', col_by, '" in dataset(x)')
-	}
-	
-	# extend margin for legend
-	mar_old <- par('mar')
-	if (is.null(mar)) {
-		mar <- mar_old
-		if (draw_legend) {
-			mar[[4]] <- mar[[4]] + 5
-		}
-	}
-	par(mar = mar)
-	
-	# reduce axis label position if it is the default and no tick marks are given
-	if (all(par('mgp') == c(3, 1, 0)) && !ticks) {
-		on.exit(par(mgp = c(3, 1, 0)))
-		par(mgp = c(1, 0, 0))
+	# use a fitting default palette
+	if (is.null(pal)) {
+		pal <- if (is.double(col)) cube_helix
+		else palette()
 	}
 	
 	# make consecutive the colors for the color legend
@@ -111,90 +99,48 @@ plot.DiffusionMap <- function(
 		col <- factor(col)
 	}
 	
-	# use a fitting default palette
-	if (is.null(pal)) {
-		col_column <- if (is.null(col)) dif[[col_by]] else col
-		pal <- if (is.double(col_column)) cube_helix else palette()
-	}
+	point_data <- cbind(
+		as.data.frame(flipped_dcs(eigenvectors(dif), dims)),
+		Colour     = col,
+		ColourExpl = get_explicit_col(col, pal, col_na, col_limits),
+		Projection = factor(rep('old', nrow(eigenvectors(dif))), c('old', 'new')))
+	rm(col)
 	
-	# colors to assign to plot rows
-	col_plot <- col
-	if (is.double(col))
-		col_plot <- continuous_colors(col, pal, col_limits)
-	else if (is.factor(col_plot))
-		col_plot <- as.integer(col_plot)
-	
-	# limit pal to number of existing colors to maybe attach col_new
-	length_pal <-
-		if (is.factor(col))
-			length(levels(col))
-		else if (is.integer(col))
-			length(unique(col_plot))
-		else 5L
-	if (is.function(pal)) {
-		# pal is a colorRampPalette-type function
-		pal <- pal(length_pal)
-	} else {
-		# pal is a vector
-		length_pal <- min(length(pal), length_pal)
-		pal <- pal[seq_len(length_pal)]
-	}
-	
-	# set col_plot to color strings
-	if (is.integer(col_plot)) {
-		idx_wrapped <- ((col_plot - 1L) %% length_pal) + 1L
-		col_plot <- pal[idx_wrapped]
-		col_plot[is.na(col_plot)] <- col_na
-	}
-	
-	# attach the new_dcs and col_new parameters to data and colors
-	point_data <- flipped_dcs(dif, dims)
 	if (!is.null(new_dcs)) {
-		point_data <- rbind(point_data, flipped_dcs(new_dcs, dims))
-		if (length(col_new) > 1L) {
-			if (length(col_new) != nrow(new_dcs)) stop('either provide one col_new or ', nrow(new_dcs), ', not ', length(col_new))
-			if (is.null(pal_new))
-				pal_new <- if (is.double(col_new)) cube_helix else palette()
-			if (is.double(col_new))
-				col_new <- continuous_colors(col_new, pal_new)
-			
-			col_plot <- c(rep_len(col_plot, nrow(point_data) - nrow(new_dcs)),
-										col_new)
-		} else {
-			if (!is.character(col_new)) {
-				idx_wrapped <- ((col_plot - 1L) %% length_pal) + 1L
-				col_new <- pal[idx_wrapped]
-			}
-			col_plot <- c(rep_len(col_plot, nrow(point_data) - nrow(new_dcs)),
-										rep_len(col_new, nrow(new_dcs)))
-			#colorlegend
-			if (!is.double(col)) {
-				col <- factor(c(as.character(col), 'proj'))
-			}
-			pal <- c(pal, col_new)
-		}
+		point_data <- rbind(point_data, cbind(
+			as.data.frame(flipped_dcs(new_dcs, dims)),
+			Colour     = col_new,  #TODO
+			ColourExpl = get_explicit_col(col_new, pal_new, col_na, col_limits),
+			Projection = 'new'
+		))
 	}
+	
+	is_one_colour <- length(unique(point_data$Colour)) == 1L
 	
 	if (length(dims) == 2) {
-		p <- NULL
+		d1 <- names(point_data)[[1L]]
+		d2 <- names(point_data)[[2L]]
 		
-		plot(point_data, ..., col = col_plot, axes = FALSE, frame.plot = box)
-		if (ticks) {
-			r1 <- NULL
-			r2 <- NULL
-			tl <- 1
-		} else {
-			r1 <- range(point_data[, 1L])
-			r2 <- range(point_data[, 2L])
-			tl <- 0
-		}
-		plot_more(p, rescale = NULL)
-		al <- if (axes && !box) 1 else 0
-		axis(1, r1, labels = ticks, lwd = al, lwd.ticks = tl)
-		axis(2, r2, labels = ticks, lwd = al, lwd.ticks = tl)
+		a <-
+			if (is_one_colour) aes_string(d1, d2)
+			else aes_string(d1, d2, colour = 'Colour')
+		p <- ggplot(point_data, a) +
+			geom_point() +
+			theme_minimal() + theme(axis.text.x = element_blank(), axis.text.y = element_blank())
+		
+		if (!is_one_colour) p <- p +
+			if (continuous) scale_colour_gradientn(name = col_by, colours = if (is.function(pal)) pal(100) else pal)
+			else            scale_colour_manual   (name = col_by, values  = if (is.function(pal)) pal(length(unique(col))) else pal)
+		if (box)   p <- p + theme(panel.border = element_rect(fill = NA), axis.title.x = element_text(), axis.title.y = element_text())
+		if (ticks) p <- p + theme(axis.ticks = element_line(), axis.text.x  = element_text(), axis.text.y  = element_text())
+		if (axes)  p <- p + geom_rangeframe(colour = par('col'))
+		if (ticks && axes && !box) p <- p + 
+			scale_x_continuous(breaks = extended_range_breaks()(point_data[[1L]])) +
+			scale_y_continuous(breaks = extended_range_breaks()(point_data[[2L]]))
+		p <- plot_more(p, rescale = NULL)
 	} else if (length(dims) == 3L) {
 		if (interactive) {
-			p <- rgl::plot3d(point_data, ..., col = col_plot, axes = FALSE, box = FALSE)
+			p <- rgl::plot3d(point_data, ..., col = point_data$ColourExpl, axes = FALSE, box = FALSE)
 			if (axes || ticks) {
 				axtype = if (axes) 'lines' else 'cull'
 				nticks = if (ticks) 5 else 0
@@ -204,33 +150,75 @@ plot.DiffusionMap <- function(
 			plot_more(p, rescale = NULL)
 		} else {
 			rescale <- NULL
-			if (!ticks) {  # -> scatterplot3d's pretty() should not mess things up
-				rescale <- list(from = range(point_data, na.rm = TRUE, finite = TRUE), to = c(0,1))
-				point_data <- rescale_mat(point_data, from = rescale$from, to = rescale$to)
+			if (!ticks) for (d in seq_along(dims)) {  # -> scatterplot3d's pretty() should not mess things up
+					point_data[, d] <- scales::rescale(point_data[, d])
 			}
+			
+			mar <- list(...)$mar
+			if (is.null(mar)) mar <- par('mar')
+			old_mar <- mar; on.exit(par(mar = old_mar))
+			if (draw_legend) mar[[4]] <- mar[[4]] + 5
+			col <- if (is_one_colour) par('col') else point_data$ColourExpl
 			p <- scatterplot3d(
-				point_data, ..., color = col_plot, mar = mar,
+				point_data[, 1:3], ..., color = col, mar = mar,
 				axis = axes || box || ticks, lty.axis = if (axes || box) 'solid' else 'blank',
 				box = box, tick.marks = ticks)
+			rm(mar)
 			plot_more(p, rescale = rescale)
+			
+			if (draw_legend) {
+				args <- c(list(col_legend, pal = pal, main = legend_main), legend_opts)
+				if (interactive) {
+					rgl::bgplot3d({
+						plot.new()
+						do.call(colorlegend, args)
+					})
+				} else {
+					do.call(colorlegend, args)
+				}
+			}
 		}
 	} else stop(sprintf('dims is of wrong length (%s): Can only handle 2 or 3 dimensions', dims))
 	
-	if (draw_legend) {
-		col_legend <- if (is.double(col) && !is.null(col_limits)) col_limits else col
-		args <- c(list(col_legend, pal = pal, main = legend_main), legend_opts)
-		if (interactive) {
-			rgl::bgplot3d({
-				plot.new()
-				do.call(colorlegend, args)
-			})
+	if (length(dims) == 2) p else invisible(p)
+}
+
+
+get_explicit_col <- function(col, pal, col_na, col_limits) {
+	# if nothing is given, return one colour
+	if (is.null(col)) return(par('col'))
+	
+	# if we have continuous colour, we are done.
+	if (is.double(col))
+		return(continuous_colors(col, pal, col_limits))
+	
+	# get palette length and convert col to consecutive integers
+	length_pal <-
+		if (is.factor(col))
+			length(levels(col))
+		else if (is.integer(col))
+			length(unique(col))
+		else stopifnot(is.character(col))
+	if (is.factor(col))
+		col <- as.integer(col)
+	
+	# map integers to strings if necessary
+	if (is.integer(col)) {
+		if (is.function(pal)) {
+			# pal is a colorRampPalette-type function
+			pal <- pal(length_pal)
 		} else {
-			do.call(colorlegend, args)
+			# pal is a vector
+			length_pal <- min(length(pal), length_pal)
+			pal <- pal[seq_len(length_pal)]
 		}
+		
+		idx_wrapped <- ((col - 1L) %% length_pal) + 1L
+		col <- pal[idx_wrapped]
+		col[is.na(col)] <- col_na
 	}
 	
-	par(mar = mar_old)
-	invisible(p)
+	col
 }
 
 #' @importFrom Biobase varLabels exprs
