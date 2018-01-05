4fb469ccd4a78268f4f7766bbd6531739d7677ba
Philipp A <flying-sheep@web.de>
2018-01-05 13:10:31 +0100

    added some C++ testing

---
+++ tests/test_main.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#include <Rembedded.h>
+#include <Rcpp.h>
+
+using namespace Rcpp;
+
+// have to replicate this here, as RcppExports isn’t a header
+List knn_asym(const NumericMatrix data, const size_t k, const std::string distance);
+
+void setup_packages() {
+	Environment base("package:base");
+	Function library = base["library"];
+	
+	library("Rcpp");
+	library("Matrix");
+}
+
+NumericMatrix exprs(RObject exprset) {
+	Environment assay_data = exprset.attr("assayData");
+	return assay_data["exprs"];
+}
+
+void main_impl() {
+	setup_packages();
+	
+	Environment global = Environment::global_env();
+	
+	Environment utils("package:utils");
+	Function load_data = utils["data"];
+	
+	load_data("guo_norm", Named("package", "destiny"));
+	NumericMatrix guo = exprs(global["guo_norm"]);
+	
+	List knns = knn_asym(guo, 5, "euclidean");
+	NumericMatrix dists = knns["dist"];
+	std::cout << dists << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+	char* argv_r[] = {argv[0], (char*)"--vanilla", (char*)"--slave"};
+	Rf_initEmbeddedR(sizeof(argv_r) / sizeof(argv_r[0]), argv_r);
+	main_impl();
+	Rf_endEmbeddedR(0);
+	return 0;
+}
