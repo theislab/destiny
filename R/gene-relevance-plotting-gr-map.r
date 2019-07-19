a52bbc0d0c29193a1cd8502252110f2d4c0d3dd5
Philipp A <flying-sheep@web.de>
2019-07-19 16:07:51 +0200

    WIP gr_plotting

--- R/gene-relevance-plotting-gr-map.r
+++ R/gene-relevance-plotting-gr-map.r
@@ -26,6 +26,7 @@ setMethod('plot_gene_relevance', c('GeneRelevance', 'missing'), function(coords,
 #' @importFrom ggplot2 geom_point
 #' @importFrom ggplot2 scale_color_manual
 #' @importFrom ggplot2 ggtitle
+#' @importFrom Biobase featureNames
 #' @importFrom utils head
 plot_gene_relevance_impl <- function(relevance_map, ..., iter_smooth, n_top, genes, dims, pal, col_na, limit) {
 	stopifparams(...)
@@ -35,36 +36,37 @@ plot_gene_relevance_impl <- function(relevance_map, ..., iter_smooth, n_top, gen
 	if (!is.numeric(iter_smooth) || length(iter_smooth) != 1L) stop('iter_smooth needs to be an integer(1)')
 	if (!is.numeric(n_top)       || length(n_top) != 1L      ) stop(      'n_top needs to be an integer(1)')
 	
-	scores <- NULL
-	if (is.character(genes)) {
-		found <- sapply(genes, function(id) length(grep(id, colnames(partials_norm))) > 0)
-		gene_ids <- genes[found]
-		gene_ids_extended <- c(gene_ids, setdiff(colnames(partials_norm), gene_ids))
-	} else if (is.null(genes)) {
-		# gene with top n norm for each cell
-		genes_max <- if (n_top == 1L) {
-			colnames(partials_norm)[apply(partials_norm, 1L, function(cell) which.max(cell))]
-		} else {
-			genes_ord <- t(apply(partials_norm, 1L, function(cell) {
-				cell <- setNames(cell, colnames(partials_norm))
-				names(cell[order(cell, decreasing = TRUE)[seq_len(n_top)]])
-			}))
-			as.vector(genes_ord)
-		}
-		counts <- as.data.frame(table(genes_max), stringsAsFactors = FALSE)
-		counts <- counts[order(counts$Freq, decreasing = TRUE), ]
-		gene_ids <- gene_ids_extended <- counts[, 'genes_max']
-		scores   <- counts[, 'Freq'] / sum(counts$Freq)
-		names(scores) <- gene_ids
+	all_genes <- featureNames(relevance_map)
+	if (is.null(genes)) {
+		genes <- all_genes
+	} else if (is.character(genes)) {
+		found <- sapply(genes, function(id) length(grep(id, all_genes)) > 0)
+		genes <- genes[found]
 	} else {
-		gene_ids <- colnames(partials_norm)[genes]
-		gene_ids_extended <- c(gene_ids, setdiff(colnames(partials_norm), gene_ids))
+		genes <- all_genes[genes]
 	}
+
+	# gene with top n norm for each cell
+	genes_max <- if (n_top == 1L) {
+		all_genes[apply(partials_norm, 1L, function(cell) which.max(cell))]
+	} else {
+		genes_ord <- t(apply(partials_norm, 1L, function(cell) {
+			cell <- setNames(cell, all_genes)
+			names(cell[order(cell, decreasing = TRUE)[seq_len(n_top)]])
+		}))
+		as.vector(genes_ord)
+	}
+	counts <- as.data.frame(table(genes_max), stringsAsFactors = FALSE)
+	counts <- counts[order(counts$Freq, decreasing = TRUE), ]
+	counts_valid <- counts[counts$genes_max %in% genes, ]
+	gene_ids <- counts_valid$genes_max
+	scores <- counts_valid$Freq / sum(counts_valid$Freq)
+	names(scores) <- gene_ids
 	
 	num_top <- min(5L, length(gene_ids))
 	top_n_cell_text <- apply(partials_norm, 1L, function(cell) {
 		idxs <- head(order(cell, decreasing = TRUE), num_top)
-		names <- colnames(partials_norm)[idxs]
+		names <- all_genes[idxs]
 		txt <- sprintf('%s. %s (%.3f)', seq_len(num_top), names, cell[idxs])
 		paste(txt, collapse = '\n')
 	})
@@ -74,8 +76,8 @@ plot_gene_relevance_impl <- function(relevance_map, ..., iter_smooth, n_top, gen
 	
 	# matrix cells by n_top. might contain NAs later
 	max_genes <-
-		if (is.null(genes) && n_top > 1L) genes_ord
-		else matrix(gene_ids[apply(partials_norm[, gene_ids, drop = FALSE], 1L, which.max)], ncol = 1)
+		if (n_top > 1L) genes_ord
+		else matrix(gene_ids[apply(partials_norm, 1L, which.max)], ncol = 1)
 	# Label smoothing through graph structure
 	for (i in seq_len(iter_smooth)) {
 		max_genes <- apply(relevance_map@nn_index, 1, function(idx_neighbors) {
@@ -93,8 +95,8 @@ plot_gene_relevance_impl <- function(relevance_map, ..., iter_smooth, n_top, gen
 	}
 	# use [1] so in case of an empty row we just get an NA.
 	gene_labels <- droplevels(structure(
-		apply(max_genes, 1, function(cell) na.omit(match(cell, gene_ids_extended))[1]),
-		levels = gene_ids_extended,
+		apply(max_genes, 1, function(cell) na.omit(match(cell, gene_ids))[1]),
+		levels = rev(gene_ids),
 		class = 'factor'
 	))
 	# Try creating a palette of the required length
